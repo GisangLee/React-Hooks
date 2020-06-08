3a0f426254b3b51fae19709f8c9ee943527c6822
DaeLee <masterkorea01@naver.com>
2020-06-08 10:13:15 +0900

    usePageLeave

---
+++ src/usePageLeave/usePageLeave.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import "./usePageLeave.css";
+
+const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
+};
+
+const App = () => {
+  const begForLife = () => console.log("Plz don't Leave");
+  useBeforeLeave(begForLife);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
